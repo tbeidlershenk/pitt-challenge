5aab3fee3bbdbfc0dfd0d4725c8d3261eab3be2c
Tobias <tab197@pitt.edu>
2022-10-22 12:49:43 -0400

    Flask app

---
+++ content/Model/App/flask-app/test.py
@@ -0,0 +1,8 @@
+from os import lseek
+import requests
+import sys
+ 
+path = sys.argv[1]
+resp = requests.post("http://localhost:5000/predict",
+                   files={'file': open(path, 'rb')})
+print(resp.text)
