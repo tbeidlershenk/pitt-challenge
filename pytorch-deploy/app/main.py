37c34b2bf25b141c92a3a172599bd34f3fa0bd8c
Jeffrey Zheng <jeffreyzheng12@gmail.com>
2022-12-26 04:00:27 -0500

    Pytorch Model Hosted on Heroku

---
+++ pytorch-deploy/app/main.py
@@ -0,0 +1,47 @@
+import traceback
+from flask import Flask, request, jsonify
+from app.torch_utils import img_to_tensor, predict_img
+import pandas as pd
+from app.ConvNeuralNetwork import ConvNeuralNetwork
+
+ 
+app = Flask(__name__)
+
+ALLOWED = ['bmp', 'jpg', 'png']
+ 
+def allowed_file(filename):
+   ext = filename.rsplit('.')[1]
+   if ext in ALLOWED:
+       return True
+   return False
+ 
+@app.route('/predict', methods=['POST'])
+
+def predict():
+   if request.method == 'POST':
+       file = request.files.get('file')
+       if file is None or file.filename == "":
+           return jsonify({'error': 'no file'})
+       if not allowed_file(file.filename):
+           return jsonify({'error': 'format not supported'})
+ 
+   try:
+       # load image
+       bytes = file.read()
+       
+       # image -> tensor
+       tensor = img_to_tensor(bytes)
+       print(tensor.shape)
+       # predict
+       prediction = predict_img(tensor)
+       top_8_df = pd.read_csv("app/top_8_annotations.csv")
+       df_class_name = top_8_df[["Name", "target"]]
+       df_class_name.drop_duplicates(inplace=True)
+       dictionary = dict(zip(df_class_name.target, df_class_name.Name))
+
+       data = {'prediction': prediction, 'class_name': dictionary[prediction]}
+       return jsonify(data)
+   except Exception as e:
+       print(str(e))
+       print(traceback.format_exc())
+       return jsonify({'error': 'error during prediction'})
---
+++ pytorch-deploy/app/torch_utils.py
@@ -0,0 +1,66 @@
+import torch
+import torchvision.transforms as tsf
+from PIL import Image
+import io
+import numpy
+from torch import nn
+import torch.nn.functional as F
+
+#load model
+class ConvNeuralNetwork(nn.Module):
+   def __init__(self):
+       super().__init__()
+       self.cnn_layers = nn.Sequential(
+           nn.Conv2d(3, 3, kernel_size=8, stride=3, padding=1),
+           nn.BatchNorm2d(3),
+           nn.ReLU(inplace=True),
+           nn.AvgPool2d(kernel_size=2, stride=2),
+           nn.Conv2d(3, 6, kernel_size=8, stride=3, padding=1),
+           nn.BatchNorm2d(6),
+           nn.ReLU(inplace=True),
+           nn.AvgPool2d(kernel_size=2, stride=2)
+       )
+       self.linear_layers = nn.Sequential(
+           nn.Linear(150, 50),
+           #nn.Linear(1000, 100),
+           nn.Linear(50, 8)
+       )
+      
+   def forward(self, x):
+       x = self.cnn_layers(x)
+       x = x.view(x.size(0), -1)
+       x = self.linear_layers(x)
+       return F.log_softmax(x)
+ 
+   def predict(self, tensor):
+       prediction = self.forward(tensor)
+       print(prediction)
+       return torch.argmax(prediction)
+
+
+# turn image to tensor
+def img_to_tensor(img_bytes):
+   transform = tsf.Compose([
+       #tsf.Grayscale(num_output_channels=1),
+       tsf.Resize((224,224)),
+       tsf.ToTensor()
+       #tsf.Normalize((0.1307,),(0.3081,))
+   ])
+   image = Image.open(io.BytesIO(img_bytes))
+   numpy.array(image).shape
+   # reverse (white -> black)
+   tensor = 1-transform(image).unsqueeze(0)
+   print(tensor.shape)
+   #print(tensor)
+   return tensor
+
+#predict
+def predict_img(tensor):
+   guess = model.predict(tensor)
+   print(guess)
+   return guess.item()
+
+# load the model
+model = ConvNeuralNetwork()
+model.load_state_dict(torch.load("app/39_6state.pt"))
+model.eval()
